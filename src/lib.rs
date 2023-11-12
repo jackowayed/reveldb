212ebc564ebc06faf4f1aa895054ba13821a62d6
Daniel Jackoway <commits@danieljackoway.com>
2023-11-12 14:35:00 -0800

    Representing log records

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+#[repr(u8)]
+enum RecordType {
+    FULL = 1,
+    FIRST = 2,
+    MIDDLE = 3,
+    LAST = 4,
+}
+struct Record {
+    checksum: u32,     // crc32c of type and data[] ; little-endian
+    length: u16,       // little-endian
+    record_type: RecordType,   // One of FULL, FIRST, MIDDLE, LAST
+    data: Box<[u8]>
+  
+}
+
+impl Record {
+    fn new(data: &[u8], record_type: RecordType) -> Self {
+        Record {checksum: 0, // TODO
+            length: data.len() as u16,
+            record_type,
+            data: Box::from(data)}
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
