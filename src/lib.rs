a06757ca0ba2c1dfb97091edb6ad0ad1dafc782e
Daniel Jackoway <commits@danieljackoway.com>
2023-11-12 20:04:58 -0800

    encode method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone, Debug)]
 #[repr(u8)]
 enum RecordType {
     FULL = 1,
@@ -5,6 +6,7 @@ enum RecordType {
     MIDDLE = 3,
     LAST = 4,
 }
+#[derive(Debug)]
 struct Record {
     checksum: u32,     // crc32c of type and data[] ; little-endian
     length: u16,       // little-endian
@@ -15,10 +17,19 @@ struct Record {
 
 impl Record {
     fn new(data: &[u8], record_type: RecordType) -> Self {
-        Record {checksum: 0, // TODO
+        Record {checksum: 1, // TODO
             length: data.len() as u16,
             record_type,
             data: Box::from(data)}
+    }
+
+    fn encode(&self) -> Vec<u8> {
+        let mut ret = Vec::new();
+        ret.extend(self.checksum.to_le_bytes());
+        ret.extend(self.length.to_le_bytes());
+        ret.push(self.record_type as u8);
+        ret.extend_from_slice(&self.data);
+        ret
     }
 }
 
