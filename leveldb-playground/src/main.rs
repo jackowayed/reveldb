fa40e79f2029c59feb350e4dd35d6244721d5af0
Daniel Jackoway <commits@danieljackoway.com>
2023-11-16 19:43:58 -0800

    Playground: Write more identifiable data

--- leveldb-playground/src/main.rs
+++ leveldb-playground/src/main.rs
@@ -1,24 +1,27 @@
-use std::fs;
-use std::path::Path;
 use leveldb;
 use leveldb::database::Database;
 use leveldb::iterator::Iterable;
 use leveldb::kv::KV;
-use leveldb::options::{Options,WriteOptions,ReadOptions};
+use leveldb::options::{Options, ReadOptions, WriteOptions};
+use std::fs;
+use std::path::Path;
 
 fn main() {
-
-  let path = Path::new("./dbs/basic");
-  fs::create_dir_all(path).unwrap();
-  let mut options = Options::new();
-  options.create_if_missing = true;
-  let mut database = match Database::open(path, options) {
-      Ok(db) => { db },
-      Err(e) => { panic!("failed to open database: {:?}", e) }
-  };
-  let write_opts = WriteOptions::new();
-  match database.put(write_opts, 1, &[1]) {
-      Ok(_) => { () },
-      Err(e) => { panic!("failed to write to database: {:?}", e) }
-  };
+    let path = Path::new("./dbs/basic2");
+    fs::create_dir_all(path).unwrap();
+    let mut options = Options::new();
+    options.create_if_missing = true;
+    let mut database = match Database::open(path, options) {
+        Ok(db) => db,
+        Err(e) => {
+            panic!("failed to open database: {:?}", e)
+        }
+    };
+    let write_opts = WriteOptions::new();
+    match database.put(write_opts, 0x77bbccdd, &[65, 66, 67, 88, 89, 90]) {
+        Ok(_) => (),
+        Err(e) => {
+            panic!("failed to write to database: {:?}", e)
+        }
+    };
 }
