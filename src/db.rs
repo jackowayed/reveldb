17ca510f0b1aedb9fb9cc81e927d30a29bb70397
Daniel Jackoway <commits@danieljackoway.com>
2023-11-25 20:13:11 -0800

    LogFile with one passing test

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,6 @@
-use std::{
-    collections::HashMap,
-    fs::{File, OpenOptions},
-    io::{ErrorKind, Read, Write},
-    path::Path,
-};
+use std::{collections::HashMap, fs::OpenOptions, io::Write, path::Path};
 
-use crate::log_file::{LogFile, Record};
+use crate::log_file::LogFile;
 
 pub struct RevelDB {
     log_file: LogFile,
@@ -34,7 +29,7 @@ impl RevelDB {
         Ok(db)
     }
 
-    fn put(&mut self, key: &[u8], value: &[u8]) -> std::io::Result<()> {
+    pub fn put(&mut self, key: &[u8], value: &[u8]) -> std::io::Result<()> {
         // TODO implement varint for this to work with sizes > 127
         let mut v: Vec<u8> = Vec::new();
         // TODO varint
@@ -51,6 +46,29 @@ impl RevelDB {
         // Change v from the full payload we store in the LogFile to just value
         v.drain(0..1 + 1 + key.len());
         self.memtable.insert(key.to_vec(), v);
+        Ok(())
+    }
+
+    pub fn get(&self, key: &[u8]) -> Option<&[u8]> {
+        self.memtable.get(key).map(|vec| vec.as_slice())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tempdir::TempDir;
+
+    use super::*;
+
+    const VAL1: [u8; 8] = [1, 2, 3, 4, 5, 5, 5, 9];
+
+    #[test]
+    fn test_put() -> std::io::Result<()> {
+        let td = TempDir::new("foobar")?;
+        let mut db = RevelDB::new(td.path())?;
+        let key: [u8; 3] = [65, 66, 67];
+        db.put(&key, &VAL1)?;
+        assert_eq!(Some(VAL1.as_slice()), db.get(&key));
         Ok(())
     }
 }
